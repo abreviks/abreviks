0a331a3e95e804505ce1d2ef2c7b76b47f626869
Andrew Schimmel <schimd@Andrews-MacBook-Pro.local>
2022-11-02 14:40:23 +0100

    initial commit of Character Counter

---
+++ README.md
@@ -0,0 +1,2 @@
+# Character Counter is a Python program that allows you to input a word or a sentence and gives you the number of characters in that word or string.
+
---
+++ char_count.py
@@ -0,0 +1,8 @@
+print("\nHello and welcome to the Character Counter! \n")
+print("Enter a word or sentence and get its total number of characters")
+x = input("Input: ")
+y = len(x)
+z = str(y)
+print("Number of characters:" + z)
+
+
